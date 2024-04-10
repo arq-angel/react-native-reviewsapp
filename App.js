239352220a13d8bdd8493cdd0af0b043c7728c0c
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-04-10 19:06:56 +1000

    complete custom fonts

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-
+import * as Font from "expo-font";
 import Home from "./screens/home";
+import { useState } from "react";
+import AppLoading from "expo-app-loading";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.log("error")}
+      />
+    );
+  }
 }
